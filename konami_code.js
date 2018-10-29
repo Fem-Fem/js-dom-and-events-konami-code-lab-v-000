694c3bd5542435b13d3d915f08323d3ff320d664
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 21:53:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,10 +6,9 @@ main.addEventListener('keydown', function(event) {
 
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let index = 0;
 
 function init(event) {
-  // Write your JavaScript code inside the init() function
+  let index = 0;
   const key = parseInt(event.detail || event.which);
 
   if (key === code[index]) {
