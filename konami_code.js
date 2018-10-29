a019dd98dcab4e23453f6fb08761aeed5314b8b6
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 21:43:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const main = document.body;
 
-main.addEventListener('keydown', init(event) {
-  alert('I was clicked!');
+main.addEventListener('keydown', function(event) {
+  init(event);
 });
 
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
