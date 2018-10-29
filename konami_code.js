d2bd52bc582f6a43f4917ebcf7dcdd38f01b438c
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 21:34:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,8 +3,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init(e) {
-  console.log(e)
-  console.log("hi")
   // Write your JavaScript code inside the init() function
   const key = parseInt(e.detail || e.which);
 
