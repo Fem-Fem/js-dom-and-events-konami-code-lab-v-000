d7538e0d14436674577b1d21963b7ba2429385c8
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 21:41:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,3 +1,9 @@
+const main = document.body;
+ 
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
