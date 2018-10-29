c74deaa00470f6057e9d556152630c077ca2b975
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 21:27:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -19,18 +19,18 @@ function init() {
   }
 }
 
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
-
-  if (key === alphabet[index]) {
-    index++;
-
-    if (index === alphabet.length) {
-      alert("Hurray!");
-
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
-}
+// function onKeyDownHandler(e) {
+//   const key = parseInt(e.detail || e.which);
+// 
+//   if (key === alphabet[index]) {
+//     index++;
+// 
+//     if (index === alphabet.length) {
+//       alert("Hurray!");
+// 
+//       index = 0;
+//     }
+//   } else {
+//     index = 0;
+//   }
+// }
