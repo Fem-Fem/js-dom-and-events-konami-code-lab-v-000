1eebef76ba8b9b7f3a21244e1f4abd52ced4bfd3
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-29 21:59:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,24 +1,49 @@
+// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+//
+// function init() {
+//   let index = 0;
+//
+//   document.body.addEventListener(('keydown', function(event) {
+//     const key = parseInt(event.detail || event.which);
+//
+//     if (key === code[index]) {
+//       index++;
+//
+//       if (index === code.length) {
+//         alert("Hurray!");
+//
+//         index = 0;
+//         }
+//       }
+//       else {
+//         index = 0;
+//       }
+//     });
+// }
+//
+// init();
+
+
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   let index = 0;
 
-  document.body.addEventListener(('keydown', function(event) {
-    const key = parseInt(event.detail || event.which);
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
 
-    if (key === code[index]) {
+    if (code[index] === key) {
       index++;
 
       if (index === code.length) {
-        alert("Hurray!");
+        alert('Hurray!');
 
         index = 0;
-        }
       }
-      else {
-        index = 0;
-      }
-    });
+    } else {
+      index = 0;
+    }
+  });
 }
 
 init();
